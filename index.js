180f1ec577a8b64b5a144ef659fa639fc3e9c1bf
unipine <unipine29@gmail.com>
2022-08-26 03:22:59 +0900

    feat: finish processing csv

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Write your answer here
+const csv = require("csv-parser");
+const fs = require("fs");
+
+function solution() {
+  const portfolio = {};
+  const WITHDRAWAL = "WITHDRAWAL";
+
+  fs.createReadStream("data/transactions.csv")
+    .pipe(csv())
+    .on("data", function ({ timestamp, transaction_type, token, amount }) {
+      if (!portfolio[token]) portfolio[token] = 0;
+      if (transaction_type === WITHDRAWAL) amount = -amount;
+      portfolio[token] += +amount;
+    })
+    .on("end", function async() {
+      console.log(portfolio);
+    });
+}
+
+solution();
