ebf4ec56b8b1738e047bc1bf9de9e08792799f7b
unipine <unipine29@gmail.com>
2022-08-26 03:44:32 +0900

    feat: add converting crypto to usd

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 const csv = require("csv-parser");
 const fs = require("fs");
+const axios = require("axios");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const API_KEY = process.env.API_KEY;
+const SERVICE_ENDPOINT = "https://min-api.cryptocompare.com/data/price";
 
 const now =
   typeof performance === "object" && performance.now
     ? performance.now.bind(performance)
     : Date.now.bind(Date);
+
+const getRate = (token) =>
+  axios
+    .get(`${SERVICE_ENDPOINT}?fsym=${token}&tsyms=USD&api_key=${API_KEY}`)
+    .then(({ data }) => ({ token, rate: data["USD"] }))
+    .catch((error) => {
+      console.error(error);
+      return false;
+    });
 
 function solution() {
   const portfolio = {};
@@ -21,8 +37,27 @@ function solution() {
       portfolio[token] += +amount;
     })
     .on("end", function async() {
-      console.info("[END] ", now() - start);
-      console.log(portfolio);
+      console.info("[END] ", now() - start, "\n");
+      console.info("Portfolio (crypto): ");
+      Object.keys(portfolio).forEach((token) =>
+        console.log(`${token}: ${portfolio[token]}`)
+      );
+
+      const tokens = Object.keys(portfolio);
+      const promises = tokens.map((token) => getRate(token));
+
+      console.info("\nPortfolio (USD): ");
+      Promise.all(promises).then((result) => {
+        result.forEach((item) =>
+          item
+            ? console.log(
+                `${item.token}: ${portfolio[item.token] * item.rate} USD`
+              )
+            : console.warn(
+                "An error occurred while getting rates from https://www.cryptocompare.com/"
+              )
+        );
+      });
     });
 }
 
